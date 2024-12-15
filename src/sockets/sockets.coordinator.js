c121c90f99df3a867714add78412eb3f6241ac68
LuisRFuentes54 <luisrf0540@gmail.com>
2024-12-15 11:17:42 -0400

    feat: Add websocket config

--- src/sockets/sockets.coordinator.js
+++ src/sockets/sockets.coordinator.js
@@ -1,4 +1,5 @@
 const WebSocket = require('ws');
+const users = require("../../db.json");
 
 const socketServer = {};
 
@@ -10,13 +11,18 @@ socketServer.init = (server) => {
     // Manejador de eventos para nuevas conexiones WebSocket
     wss.on('connection', (ws) => {
         console.log('Nuevo cliente conectado');
-        ws.send("Conectado al servidor de WebSocekts");
+        ws.send("Conectado al servidor de WebSocekts del taller 4 de program web");
 
         // Manejador de eventos para mensajes recibidos
         ws.on('message', (message) => {
             console.log(`Mensaje recibido: ${message}`);
-            // Aquí puedes procesar el mensaje y enviar una respuesta
-            ws.send("Conectado al servidor de WebSocekts");
+            for (const user of users) {
+                if (user.key == message) {
+                    ws.send(`Mensaje recibido: ${user.name}`);
+                    return;
+                }
+            }
+            ws.send("Error: Clave invalida");
         });
 
         // Manejador de eventos para desconexiones
